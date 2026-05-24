6e3303250e48d0295b8543de097d90e3ecb82a52
xiangxiang <xiangxiang@openafp.net>
2026-05-24 23:19:25 +0800

    release v0.36.3: update version

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-VERSION="${OPENAFP_VERSION:-v0.36.2}"
+VERSION="${OPENAFP_VERSION:-v0.36.3}"
 REPO="https://gitee.com/openafp/openafp-public"
 GH_REPO="https://github.com/openafp-net/openafp"
 CONFIG_DIR="${HOME}/.openafp"
